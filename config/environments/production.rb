7f9689e8d1e7ea0e8fb2c836fcd25c84d1d612e5
lokeshkumarvvrp <lokeshvvr17@gmail.com>
2023-04-23 10:45:49 +0530

    deployment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,9 +6,9 @@ Rails.application.configure do
   
 
 
-config.hosts << "localhost:3000"
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+
 
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
